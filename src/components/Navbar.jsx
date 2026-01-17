67f4f15f0522ff919ed6b7d7a9ef274944503bee
Ayush Bahuguna <Ayushbahuguna006@gmail.com>
2026-01-18 00:36:05 +0530

    navbar components rendered

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import { navLinks } from '../../constants'
 
 const Navbar = () => {
   return (
-    <div>
-      Navbar
-    </div>
+    <nav>
+        <div className='min-h-screen bg-black'> 
+            <a href="#home" className='flex justify-center gap-2 items-center p-3 text-white'>
+               <img src="./src/assets/logo.png" alt="Logo" />
+               <p className='text-xl font-semibold'>Velvet Pour</p> </a>
+               <ul className='flex text-white justify-center gap-5'>
+                {navLinks.map((link)=>(<li key={link.id}><a href={`#${link.id}`}>{link.title}</a></li>))}
+               </ul>
+
+           
+        </div>
+    </nav>
   )
 }
 
