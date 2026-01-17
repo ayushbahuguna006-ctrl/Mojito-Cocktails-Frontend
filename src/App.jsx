0663fdb41a792660c0cd3c8c2bb6e10bf2b4f285
Ayush Bahuguna <Ayushbahuguna006@gmail.com>
2026-01-17 22:54:53 +0530

    Project Initialized

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
+gsap.registerPlugin(useGSAP);
+
+
+
+function App() {
+ 
+
+  return (
+    <>
+      HII
+    </>
+  )
+}
+
+export default App
