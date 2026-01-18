0335198b08ef535b872833b5299d044ea70af291
Ayush Bahuguna <Ayushbahuguna006@gmail.com>
2026-01-19 00:31:45 +0530

    Button enhanced

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -89,7 +89,7 @@ two.to('.drink2',{
                             <h1 className='text-yellow-200 text-4xl font-bold mt-3 md:absolute md:left-[10vh]   md:block md:top-[45vh] absolute left-[20vh] top-[30vh] hidden'>Sip The Spirit </h1>
                             <h1 className='text-yellow-200 text-4xl font-bold mt-1 md:absolute md:left-[10vh]   md:block md:top-[51vh]  left-[23vh] hidden'>Of Summer </h1></div>
                             <div className='subtitle text-white  md:absolute md:top-[55vh] md:right-[10vh] p-2 md:block'><div>Every cocktail on our menu,</div> is a blend of premium ingredients, creative flair, <div>and timeless recipes — designed to delight your senses
-                               <div className='mt-5'><a href="#cocktails" className='text-red-500 hover:bg-blue-500 hover:rounded-xl hover:p-2 hover:text-black hover:font-bold transition-all duration-75 ease-in'>View Cocktails</a><div className='dot bg-red-200  h-0.5 w-0.5'></div><div><img src="/drink2.png" className='drink2 md:absolute md:right-[70vh] md:top-[1vh]' alt="drink" /></div></div> </div> </div>
+                               <div className='mt-5'><a href="#cocktails" className='text-red-900 hover:bg-blue-200 hover:rounded-xl hover:p-2 hover:text-red-400 hover:font-bold transition-all duration-75 ease-in'>View Cocktails</a><div className='dot bg-red-200  h-0.5 w-0.5'></div><div><img src="/drink2.png" className='drink2 md:absolute md:right-[70vh] md:top-[1vh]' alt="drink" /></div></div> </div> </div>
                         </div> 
                     </div>
                  </div>
