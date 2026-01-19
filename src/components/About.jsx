662a283cb396f45709cb1bfc24fb256e8b711253
Ayush Bahuguna <Ayushbahuguna006@gmail.com>
2026-01-20 00:09:07 +0530

    About sections updated

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -51,7 +51,7 @@ const About = () => {
 });
   return (
     <div className='bg-[url("/noise.png")] min-h-screen overflow-hidden'>
-      <div className='flex  justify-evenly items-center'>
+      <div className='flex  justify-center md:gap-20 items-center'>
         <div className='sun p-4'>
           <a href='#cocktails' className='text-black rounded-4xl px-3 text-sm p-2 bg-white cursor-pointer hover:text-red-500 '>Best Cocktails</a>
              <div className='text-white  text-4xl font-black mt-3'><p>Where every Detail </p>
@@ -63,7 +63,7 @@ const About = () => {
               <p>Every cocktail we serve is a reflection of our </p>
               <p>obsession with detail — from the first muddle to the</p>
               <p>final garnish. That care is what turns a simple drink</p>
-              <p>nto something truly memorable.</p>
+              <p>into something truly memorable.</p>
               <div className='flex justify-evenly w-1/2 pt-4'><p className='text-yellow-500 font-bold text-2xl'>4.5</p><p className='pt-2'> / </p><p className='pt-2.5'>5</p></div>
               <div><p className='text-sm font-light'>More than 12000+ customers</p><div className='bg-stone-400 mt-5 p-5 rounded-xl w-full flex justify-center  items-center md:w-1/2 h-5'><img src="/profile1.png" className='h-7' alt="" /><img src="/profile2.png" className='h-7' alt="" /><img src="/profile3.png" alt="" className='h-7' /><img src="/profile4.png" className='h-7' alt="" /></div></div>
         </div>
