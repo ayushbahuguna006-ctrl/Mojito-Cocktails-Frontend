76cf640967b9b7d1283581f10439399d3c14c035
Ayush Bahuguna <Ayushbahuguna006@gmail.com>
2026-01-20 23:54:26 +0530

    UI Enhanced

--- src/components/Cocktails.jsx
+++ src/components/Cocktails.jsx
@@ -78,7 +78,8 @@ const Cocktails = () => {
     )})
   return (
     <>
-      <div id='cocktails' className='tails  min-h-screen bg-[url("/noise.png")] flex flex-col md:flex-row  justify-evenly items-center' >
+    <div id='cocktails'></div><br/><br/><br/>
+      <div  className='tails  min-h-screen bg-[url("/noise.png")] flex flex-col md:flex-row  justify-evenly items-center' >
          <div className='p-4'  >
               <p className='text-white'>Most Popular Cocktails</p><br/>
               {cocktailLists.map(({name,detail,country,price})=>(
